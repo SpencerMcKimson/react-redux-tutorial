c57581d0a4ad7449d2460f96de7f9021eda956aa
Spencer McKimson <spencer.mckimson@gmail.com>
2018-02-08 16:26:20 -0800

    add index files for reducers, actions, store and constants as well as an entry point to the app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import store from "./js/store/index";
+import { addArticle } from "./js/actions/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+window.store = store;
+
+window.addArticle = addArticle;
